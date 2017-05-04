78ccc54f61de53c468d588a545838cdcf850589a
Tara Mason <tara.mason@gmail.com>
2017-05-04 13:45:07 -0600

    fixed env files

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,12 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + "?ssl=true"
+    connection: {
+      user: process.env.DB_USER,
+      password:process.env.DB_PASSWORD,
+      database:process.env.DB,
+      host:process.env.DB_HOST,
+      port:process.env.DB_PORT
+    }
   }
 };
